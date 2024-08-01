051f609a95a6890991731ec25ec21def0e6c2dfb
mes service <mes_service@daum.net>
2024-08-01 09:11:25 +0900

    bottom bar 위치 변경

--- packages/app/features/marker/marker-list.tsx
+++ packages/app/features/marker/marker-list.tsx
@@ -1,16 +1,35 @@
-import { Button, Paragraph, XStack, YStack, SizableText, Separator, Stack } from '@my/ui'
-import { ChevronLeft, PlusCircle, Trash, Piano } from '@tamagui/lucide-icons'
+import {
+  Button,
+  Paragraph,
+  XStack,
+  YStack,
+  SizableText,
+  Separator,
+  Stack,
+  useToastController,
+  Sheet,
+  Anchor,
+} from '@my/ui'
+import {
+  ChevronLeft,
+  PlusCircle,
+  Trash,
+  Piano,
+  ChevronDown,
+  ChevronUp,
+} from '@tamagui/lucide-icons'
+import { useState } from 'react'
 import { useLink, useRouter } from 'solito/navigation'
 
 export function MarkerView() {
   const router = useRouter()
 
   const linkProps = useLink({
-    href: `/marker/addMarker`,
+    href: `/marker/selectMarker`,
   })
 
   return (
-    <YStack f={1} ai="center" gap="$4" w="100%" h="100%">
+    <YStack f={1} ai="center" gap="$4" w="100%" h="100%" p="$2">
       <XStack
         backgroundColor="$blue10"
         p="$2.5"
@@ -56,9 +75,68 @@ export function MarkerView() {
       </Stack>
       <XStack f={2} jc="space-between" ai="flex-end" gap="$4" p={2} w="100%" m={2}>
         <Button {...linkProps} icon={PlusCircle}></Button>
-        <Button icon={ChevronLeft} onPress={() => router.back()}></Button>
+        <SheetDemo />
         <Button icon={Trash} onPress={() => router.back()}></Button>
       </XStack>
     </YStack>
+  )
+}
+
+function SheetDemo() {
+  const toast = useToastController()
+
+  const [open, setOpen] = useState(false)
+  const [position, setPosition] = useState(0)
+
+  return (
+    <>
+      <Button
+        size="$6"
+        icon={open ? ChevronDown : ChevronUp}
+        circular
+        onPress={() => setOpen((x) => !x)}
+      />
+      <Sheet
+        modal
+        animation="medium"
+        open={open}
+        onOpenChange={setOpen}
+        snapPoints={[80]}
+        position={position}
+        onPositionChange={setPosition}
+        dismissOnSnapToBottom
+      >
+        <Sheet.Overlay animation="lazy" enterStyle={{ opacity: 0 }} exitStyle={{ opacity: 0 }} />
+        <Sheet.Handle bg="$gray8" />
+        <Sheet.Frame ai="center" jc="center" gap="$10" bg="$color2">
+          <XStack gap="$2">
+            <Paragraph ta="center">Made by</Paragraph>
+            <Anchor col="$blue10" href="https://twitter.com/natebirdman" target="_blank">
+              @natebirdman,
+            </Anchor>
+            <Anchor
+              color="$purple10"
+              href="https://github.com/tamagui/tamagui"
+              target="_blank"
+              rel="noreferrer"
+            >
+              give it a ⭐️
+            </Anchor>
+          </XStack>
+
+          <Button
+            size="$6"
+            circular
+            icon={ChevronDown}
+            onPress={() => {
+              setOpen(false)
+              toast.show('Sheet closed!', {
+                message: 'Just showing how toast works...',
+              })
+            }}
+          />
+        </Sheet.Frame>
+      </Sheet>
+    </>
   )
 }
