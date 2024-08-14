aeb76923e16eeab9f976c053414fa01d468eb1d8
hyeon3051 <hyeon3051@gmail.com>
2024-08-14 13:28:42 +0900

    feat: Update route-add.tsx to improve route creation form

--- packages/app/features/route/route-add.tsx
+++ packages/app/features/route/route-add.tsx
@@ -1,9 +1,23 @@
-import { Button, XStack, YStack, Input, TextArea, H3, H6, H5, useToastController, Square, Slider, SliderProps,Separator } from '@my/ui'
+import {
+  Button,
+  XStack,
+  YStack,
+  Input,
+  TextArea,
+  H3,
+  H6,
+  H5,
+  useToastController,
+  Square,
+  Slider,
+  SliderProps,
+  Separator,
+} from '@my/ui'
 import TamaIcon from 'packages/app/ui/Icon'
 import { useContext, useEffect, useState } from 'react'
-import { useLink, useParams, useRouter } from 'solito/navigation'
+import { useParams, useRouter } from 'solito/navigation'
 import { fileState, fileDispatch } from 'packages/app/contexts/mapData/fileReducer'
-import { Marker, Route } from 'packages/app/types/type'
+import { Route } from 'packages/app/types/type'
 import 'react-native-get-random-values'
 import { v4 as uuidv4 } from 'uuid'
 export function AddRouteView() {
@@ -20,7 +34,7 @@ export function AddRouteView() {
     description: '',
     path: [],
     lineWidth: 3,
-    lineColor: "#fbfbfb"
+    lineColor: '#fbfbfb',
   })
 
   useEffect(() => {
@@ -35,12 +49,12 @@ export function AddRouteView() {
         id: id,
         title: title,
         description: description,
-        path: path
+        path: path,
       }))
     }
   }, [params])
 
-  const { title, description,  id:routeId, lineColor, lineWidth } = routeInfo
+  const { title, description, id: routeId, lineColor, lineWidth } = routeInfo
 
   const router = useRouter()
 
@@ -57,13 +71,13 @@ export function AddRouteView() {
     }))
   }
 
-  const onWidthChange = (value) =>{
+  const onWidthChange = (value) => {
     console.log()
-    setRouteInfo((prev)=>({
+    setRouteInfo((prev) => ({
       ...prev,
-      lineWidth: value[0]
+      lineWidth: value[0],
     }))
-  } 
+  }
 
   const handleRemove = () => {
     if (routeIdx === -1 && routeId) return
@@ -76,11 +90,11 @@ export function AddRouteView() {
 
   const handleChange = () => {
     if (routeIdx !== -1 && routeId) {
-      dispatch({ type: 'EDIT_ROUTE', payload: { routeId: routeId, route: routeInfo }})
+      dispatch({ type: 'EDIT_ROUTE', payload: { routeId: routeId, route: routeInfo } })
     } else {
       dispatch({
         type: 'ADD_ROUTE',
-        payload: {  routeId: routeId },
+        payload: { routeId: routeId },
       })
     }
     toast.show('Sheet closed!', {
@@ -91,61 +105,61 @@ export function AddRouteView() {
 
   return (
     <>
-    <YStack f={1} gap="$1" w="100%" h="100%" jc="flex-start" p="$2">
-      <XStack gap="$4" p="$2" w="100%" m={20} ai="center">
-        <YStack>
-          <H3>{title || 'example'}</H3>
-          <H6>Lorem ipsum</H6>
-        </YStack>
-      </XStack>
-      <XStack gap="$4" p="$2" w="80%" ml={20} ai="center">
-        <Separator backgroundColor={lineColor} borderColor={lineColor} borderWidth={lineWidth}/>
-      </XStack>
-      <YStack gap="$4" p="$2" w="80%" ml={20}>
-        <H5>name</H5>
-        <Input onChangeText={onNameChange} value={title} />
-      </YStack>
-      <YStack gap="$4" p="$2" w="80%" ml={20}>
-        <H5>description</H5>
-        <TextArea onChangeText={onDescriptionChange} value={description} />
-      </YStack>
-      <YStack gap="$4" p="$2" w="80%" ml={20}>
-        <H5>color</H5>
-        <XStack gap="$2" jc="space-between">
-          {['$red10', '$blue10', '$purple10', '$green10'].map((color, index) => (
-            <Square
-              key={index}
-              size="$6"
-              hoverStyle={{
-                scale: 1.5,
-              }}
-              pressStyle={{
-                scale: 0.9,
-              }}
-              onPress={() =>
-                setRouteInfo({
-                  ...routeInfo,
-                  lineColor: color,
-                })
-              }
-              backgroundColor={color}
-              elevation={color === '$red10' ? '$5' : undefined}
-            />
-          ))}
+      <YStack f={1} gap="$1" w="100%" h="100%" jc="flex-start" p="$2">
+        <XStack gap="$4" p="$2" w="100%" m={20} ai="center">
+          <YStack>
+            <H3>{title || 'example'}</H3>
+            <H6>Lorem ipsum</H6>
+          </YStack>
         </XStack>
-      </YStack>
+        <XStack gap="$4" p="$2" w="80%" ml={20} ai="center">
+          <Separator backgroundColor={lineColor} borderColor={lineColor} borderWidth={lineWidth} />
+        </XStack>
+        <YStack gap="$4" p="$2" w="80%" ml={20}>
+          <H5>name</H5>
+          <Input onChangeText={onNameChange} value={title} />
+        </YStack>
+        <YStack gap="$4" p="$2" w="80%" ml={20}>
+          <H5>description</H5>
+          <TextArea onChangeText={onDescriptionChange} value={description} />
+        </YStack>
+        <YStack gap="$4" p="$2" w="80%" ml={20}>
+          <H5>color</H5>
+          <XStack gap="$2" jc="space-between">
+            {['$red10', '$blue10', '$purple10', '$green10'].map((color, index) => (
+              <Square
+                key={index}
+                size="$6"
+                hoverStyle={{
+                  scale: 1.5,
+                }}
+                pressStyle={{
+                  scale: 0.9,
+                }}
+                onPress={() =>
+                  setRouteInfo({
+                    ...routeInfo,
+                    lineColor: color,
+                  })
+                }
+                backgroundColor={color}
+                elevation={color === '$red10' ? '$5' : undefined}
+              />
+            ))}
+          </XStack>
+        </YStack>
         <YStack gap="$4" p="$2" w="80%" ml={20}>
           <H5>width</H5>
-          <SimpleSlider onValueChange={onWidthChange}/>
+          <SimpleSlider onValueChange={onWidthChange} />
         </YStack>
 
-      <XStack f={1} jc="space-between" ai="flex-end" gap="$4" p={2} w="100%" m={2}>
-        <Button icon={<TamaIcon iconName="PlusCircle" />} onPress={handleChange}></Button>
-        <Button icon={<TamaIcon iconName="ChevronLeft" />} onPress={() => router.back()}></Button>
-        <Button icon={<TamaIcon iconName="Trash" />} onPress={handleRemove}></Button>
-      </XStack>
-    </YStack>
-</>
+        <XStack f={1} jc="space-between" ai="flex-end" gap="$4" p={2} w="100%" m={2}>
+          <Button icon={<TamaIcon iconName="PlusCircle" />} onPress={handleChange}></Button>
+          <Button icon={<TamaIcon iconName="ChevronLeft" />} onPress={() => router.back()}></Button>
+          <Button icon={<TamaIcon iconName="Trash" />} onPress={handleRemove}></Button>
+        </XStack>
+      </YStack>
+    </>
   )
 }
 
