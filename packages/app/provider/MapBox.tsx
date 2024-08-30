2c2f02f2dfd67cfeba8a3f6d23393c65c062d987
hyeon3051 <hyeon3051@gmail.com>
2024-08-30 15:13:08 +0900

    Optimize MapBoxComponent location and zoom level in FileView and SelectDataView components

--- packages/app/provider/MapBox.tsx
+++ packages/app/provider/MapBox.tsx
@@ -1,28 +1,28 @@
 import React, { use, useEffect, useRef } from 'react'
 import MapboxGL, { Camera } from '@rnmapbox/maps'
 import { Pos } from '../types/type'
-import { useColorScheme } from 'react-native'
+import useBackgroundGeolocation from 'app/services/BackGroundGelocation'
 MapboxGL.setAccessToken(
   'sk.eyJ1IjoiaHllb24zMDUxIiwiYSI6ImNsa3YwM3BhcjBneGEzbHIweGFuNTgzZXoifQ.uvJeaDq7NLN0HyOENlWUcA'
 )
 
-const MapBoxComponent = ({
-  location,
-  children,
-  zoomLevel = 15,
-}: {
-  location: Pos
-  children: React.ReactNode
-  zoomLevel: number
-}) => {
+const MapBoxComponent = ({ location, children }: { location?: Pos; children: React.ReactNode }) => {
+  let { location: currLocation } = useBackgroundGeolocation()
   const camera = useRef<Camera>(null) // Corrected here
-  const colorScheme = useColorScheme()
-  const styleURL = colorScheme === 'dark' ? MapboxGL.StyleURL.Dark : MapboxGL.StyleURL
+  const mapRef = useRef<MapboxGL.MapView>(null)
+
+  mapRef.current?.getZoom().then((zoom) => {
+    camera.current?.setCamera({
+      zoomLevel: zoom,
+      animationDuration: 1000,
+    })
+  })
+
   useEffect(() => {
     if (location && camera.current) {
       camera.current.setCamera({
-        centerCoordinate: location[0],
-        animationDuration: 500,
+        centerCoordinate: location[0] ?? currLocation[0],
+        animationDuration: 1000,
       })
     }
   }, [location])
@@ -33,17 +33,16 @@ const MapBoxComponent = ({
         style={{ flex: 1 }}
         logoEnabled={false}
         attributionEnabled={false}
+        zoomEnabled={true}
+        ref={mapRef}
       >
-        <MapboxGL.UserLocation
-          androidRenderMode="normal"
-          animated={true}
-        />        
+        <MapboxGL.UserLocation androidRenderMode="normal" animated={true} />
         <MapboxGL.Camera
           ref={camera}
-          zoomLevel={zoomLevel}
           animationMode="easeTo"
           followUserLocation={true}
           animationDuration={1000}
+          followZoomLevel={15}
         />
         {children}
       </MapboxGL.MapView>
