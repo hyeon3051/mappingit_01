c72792adbee63b1bdc6e2cce7b6b74f7203d17c8
hyeon3051 <hyeon3051@gmail.com>
2024-09-05 14:42:32 +0900

    feat: Add ScrollView to marker-select component
    This commit adds the ScrollView component to the marker-select component in order to enable scrolling within the component. This improves the user experience by allowing users to scroll through the list of markers and select their desired options easily.

--- packages/app/features/marker/marker-select.tsx
+++ packages/app/features/marker/marker-select.tsx
@@ -1,4 +1,4 @@
-import { Button, Paragraph, XStack, YStack, Stack, Square } from '@my/ui'
+import { Button, Paragraph, XStack, YStack, Stack, Square, ScrollView } from '@my/ui'
 import { fileDispatch, fileState } from 'packages/app/contexts/mapData/fileReducer'
 import { selectedIcon } from 'packages/app/types/type'
 import TamaIcon from 'packages/app/ui/Icon'
@@ -44,92 +44,96 @@ export function SelectMarkerView() {
   useEffect(() => {}, [markerIcon])
 
   return (
-    <YStack f={1} ai="center" gap="$0" w="100%" h="100%" jc="flex-start" p="$2">
-      <Stack p="$2" gap="$5" jc="flex-start" mt="$2" w="100%">
-        <Paragraph>즐겨찾기</Paragraph>
-        <XStack gap="$3" jc="space-around">
-          {['PinOff', 'PinOff', 'PinOff', 'PinOff'].map((iconName, index) => (
-            <Button
-              key={index}
-              size="$7"
-              circular
-              iconAfter={<TamaIcon iconName={iconName} size="$6" />}
-              onPress={() => setMarkerIcon({ ...markerIcon, icon: iconName })}
-            />
-          ))}
-        </XStack>
-      </Stack>
-      <Stack p="$2" gap="$2" jc="flex-start" mt="$2" w="100%">
-        <Paragraph>마커</Paragraph>
-        <XStack gap="$2" jc="space-around" flexWrap="wrap">
-          {[
-            'PinOff',
-            'Eraser',
-            'AArrowUp',
-            'MapPinOff',
-            'MapPin',
-            'Pin',
-            'TreePine',
-            'ShoppingBag',
-          ].map((iconName: string, index) => (
-            <Button
-              key={index}
-              size="$7"
-              animation="medium"
-              circular
-              icon={
-                <TamaIcon
-                  iconName={iconName}
-                  size="$6"
-                  color={iconName === markerIcon.icon ? markerIcon.color : ''}
+    <>
+      <ScrollView>
+        <YStack f={1} ai="center" gap="$0" w="100%" h="100%" jc="flex-start" p="$2">
+          <Stack p="$2" gap="$5" jc="flex-start" mt="$2" w="100%">
+            <Paragraph>즐겨찾기</Paragraph>
+            <XStack gap="$3" jc="space-around">
+              {['PinOff', 'PinOff', 'PinOff', 'PinOff'].map((iconName, index) => (
+                <Button
+                  key={index}
+                  size="$7"
+                  circular
+                  iconAfter={<TamaIcon iconName={iconName} size="$6" />}
+                  onPress={() => setMarkerIcon({ ...markerIcon, icon: iconName })}
                 />
-              }
-              onPress={() => setMarkerIcon({ ...markerIcon, icon: iconName })}
-            />
-          ))}
-        </XStack>
-      </Stack>
-      <Stack p="$2" gap="$5" jc="flex-start" mt="$2" w="100%">
-        <Paragraph>색상</Paragraph>
-        <XStack gap="$3" jc="space-around">
-          {['$red10', '$blue10', '$purple10', '$green10'].map((color, index) => (
-            <Square
-              key={index}
-              size="$8"
-              hoverStyle={{
-                scale: 1.5,
-              }}
-              pressStyle={{
-                scale: 0.9,
-              }}
-              onPress={() =>
-                setMarkerIcon({
-                  ...markerIcon,
-                  color,
-                })
-              }
-              backgroundColor={color}
-              elevation={color === '$red10' ? '$5' : undefined}
-            />
-          ))}
-        </XStack>
-        <XStack gap="$3" jc="space-around">
-          {['$red10', '$blue10', '$purple10', '$green10'].map((color, index) => (
-            <Square
-              key={index}
-              size="$8"
-              onPress={() =>
-                setMarkerIcon({
-                  ...markerIcon,
-                  color,
-                })
-              }
-              backgroundColor={color}
-              elevation={color === '$red10' ? '$5' : undefined}
-            />
-          ))}
-        </XStack>
-      </Stack>
+              ))}
+            </XStack>
+          </Stack>
+          <Stack p="$2" gap="$2" jc="flex-start" mt="$2" w="100%">
+            <Paragraph>마커</Paragraph>
+            <XStack gap="$2" jc="space-around" flexWrap="wrap">
+              {[
+                'PinOff',
+                'Eraser',
+                'AArrowUp',
+                'MapPinOff',
+                'MapPin',
+                'Pin',
+                'TreePine',
+                'ShoppingBag',
+              ].map((iconName: string, index) => (
+                <Button
+                  key={index}
+                  size="$7"
+                  animation="medium"
+                  circular
+                  icon={
+                    <TamaIcon
+                      iconName={iconName}
+                      size="$6"
+                      color={iconName === markerIcon.icon ? markerIcon.color : ''}
+                    />
+                  }
+                  onPress={() => setMarkerIcon({ ...markerIcon, icon: iconName })}
+                />
+              ))}
+            </XStack>
+          </Stack>
+          <Stack p="$2" gap="$5" jc="flex-start" mt="$2" w="100%">
+            <Paragraph>색상</Paragraph>
+            <XStack gap="$3" jc="space-around">
+              {['$red10', '$blue10', '$purple10', '$green10'].map((color, index) => (
+                <Square
+                  key={index}
+                  size="$8"
+                  hoverStyle={{
+                    scale: 1.5,
+                  }}
+                  pressStyle={{
+                    scale: 0.9,
+                  }}
+                  onPress={() =>
+                    setMarkerIcon({
+                      ...markerIcon,
+                      color,
+                    })
+                  }
+                  backgroundColor={color}
+                  elevation={color === '$red10' ? '$5' : undefined}
+                />
+              ))}
+            </XStack>
+            <XStack gap="$3" jc="space-around">
+              {['$red10', '$blue10', '$purple10', '$green10'].map((color, index) => (
+                <Square
+                  key={index}
+                  size="$8"
+                  onPress={() =>
+                    setMarkerIcon({
+                      ...markerIcon,
+                      color,
+                    })
+                  }
+                  backgroundColor={color}
+                  elevation={color === '$red10' ? '$5' : undefined}
+                />
+              ))}
+            </XStack>
+          </Stack>
+        </YStack>
+      </ScrollView>
       <XStack f={2} jc="space-around" ai="flex-end" gap="$4" p={2} w="100%" m={2}>
         <Button
           {...(marker !== -1 ? { ...linkProps } : {})}
@@ -146,6 +150,6 @@ export function SelectMarkerView() {
           </Button>
         )}
       </XStack>
-    </YStack>
+    </>
   )
 }
