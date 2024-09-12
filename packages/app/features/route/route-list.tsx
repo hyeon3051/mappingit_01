88eb4756c718eb6ad9877cc493c94adc19a5231d
hyeon3051 <hyeon3051@gmail.com>
2024-09-12 17:30:18 +0900

    feat: Add lazy loading for images in MarkerAddView component

--- packages/app/features/route/route-list.tsx
+++ packages/app/features/route/route-list.tsx
@@ -1,4 +1,3 @@
-import { Button, XStack, SizableText, Separator, Stack } from '@my/ui'
 import { PlusCircle, FileEdit } from '@tamagui/lucide-icons'
 import MapBoxComponent from 'packages/app/provider/MapBox'
 import { useContext, useEffect, useRef, useState } from 'react'
@@ -9,15 +8,133 @@ import { fileState } from 'packages/app/contexts/mapData/fileReducer'
 import Carousel from 'react-native-reanimated-carousel'
 import { CardDemo } from 'packages/app/component/CardDemo'
 import { SheetDemo } from 'packages/app/component/SheetDemo'
-import useBackgroundGeolocation from 'packages/app/services/BackGroundGelocation'
+import { Button, XStack, SizableText, Card, Stack, YStack, Paragraph, Image } from '@my/ui'
+import { TabView, SceneMap } from 'react-native-tab-view'
+import { useWindowDimensions } from 'react-native'
+import { create } from 'zustand'
+import { Route, Pos } from 'packages/app/types/type'
 
-export function RouteView() {
-  const router = useRouter()
-  const carouselRef = useRef(null)
-  const [idx, setIdx] = useState(0)
-  const [route, setRoute] = useState<number[]>([127, 38])
+interface RouteState {
+  idx: number
+  setIdx: (idx: number) => void
+}
+
+const useMarkerState = create<RouteState>((set) => ({
+  idx: -1,
+  setIdx(idx) {
+    set({ idx })
+  },
+}))
+
+const RouteOnMap = ({ location }) => {
   const fileInfo = useContext(fileState)
-  const { location } = useBackgroundGeolocation()
+  const { idx } = useMarkerState()
+  const [route, setRoute] = useState<Route>()
+  useEffect(() => {
+    if (idx !== -1) {
+      setRoute(fileInfo?.routes[idx])
+    } else {
+      setRoute({
+        title: 'current',
+        description: 'current',
+        path: fileInfo?.currentRoute,
+        lineColor: 'black',
+        lineWidth: 2,
+      })
+    }
+  }, [idx])
+  return (
+    <MapBoxComponent location={location}>
+      {route && (
+        <>
+          <MapboxGL.PointAnnotation coordinate={[127, 37]} key="start" id="pt-ann">
+            <TamaIcon iconName="MapPin" color="$black10" size="$2" />
+          </MapboxGL.PointAnnotation>
+          <MapboxGL.PointAnnotation coordinate={location} key="end" id="pt-ann">
+            <TamaIcon iconName="MapPin" color="$black10" size="$2" />
+          </MapboxGL.PointAnnotation>
+          <MapboxGL.ShapeSource
+            id="line-source"
+            shape={{
+              type: 'Feature',
+              properties: {},
+              geometry: {
+                type: 'LineString',
+                coordinates: route?.path?.map((pos) => pos[0]) || [],
+              },
+            }}
+          >
+            <MapboxGL.LineLayer
+              id="line"
+              sourceID="line"
+              style={{
+                lineColor: route.lineColor,
+                lineWidth: route.lineWidth,
+              }}
+            />
+          </MapboxGL.ShapeSource>
+        </>
+      )}
+    </MapBoxComponent>
+  )
+}
+
+const RouteInfoView = () => {
+  const fileInfo = useContext(fileState)
+  const { idx } = useMarkerState()
+  const [route, setRoute] = useState()
+  useEffect(() => {
+    if (idx !== -1) {
+      setRoute(fileInfo?.routes[idx])
+    } else {
+      setRoute({
+        title: 'current',
+        description: 'current',
+      })
+    }
+  }, [idx])
+  const start_at = new Date()
+  const startDate = start_at.toLocaleDateString()
+  const startTime = start_at.toLocaleTimeString()
+  const end_at = new Date()
+  const endDate = end_at.toLocaleDateString()
+  const endTime = end_at.toLocaleTimeString()
+  return (
+    <>
+      <Stack zIndex={3} pos="absolute" left={0} bottom={'20%'} height="20%">
+        <Card size="$4" width="100%" height="100%" backgroundColor="$black0" mx="$2" px="$2">
+          <Card.Header padded>
+            <Paragraph></Paragraph>
+          </Card.Header>
+          <Stack
+            borderColor="$white075"
+            backgroundColor="$white075"
+            borderWidth="$1"
+            width="$15"
+            height="$20"
+          >
+            <XStack gap="$3" ai="flex-start" jc="center" px="$4">
+              <YStack alignContent="center" w="80%">
+                <SizableText size="$8">{route?.title}</SizableText>
+                <Paragraph size="$1">{route?.description}</Paragraph>
+                <Paragraph size="$1">{startDate}</Paragraph>
+                <Paragraph size="$1">{startTime}</Paragraph>
+                <Paragraph size="$1">{endDate}</Paragraph>
+                <Paragraph size="$1">{endTime}</Paragraph>
+              </YStack>
+            </XStack>
+          </Stack>
+          <Card.Footer></Card.Footer>
+        </Card>
+      </Stack>
+    </>
+  )
+}
+
+export function RouteListView() {
+  const carouselRef = useRef(null)
+  const { idx, setIdx } = useMarkerState()
+  const fileInfo = useContext(fileState)
 
   const linkProps = useLink({
     href: `/route/addRoute`,
@@ -33,65 +150,10 @@ export function RouteView() {
       carouselRef.current.scrollTo({ index })
     }
   }
-
-  const calculateZoomLevel = (start, end) => {
-    if (!start || !end) return 10 // Default zoom level
-
-    const distance = Math.sqrt(Math.pow(end[0] - start[0], 2) + Math.pow(end[1] - start[1], 2))
-
-    // Simple heuristic for zoom level based on distance
-    if (distance < 0.01) return 15
-    if (distance < 0.1) return 12
-    return 10
-  }
-
   const routes = fileInfo?.routes || []
-  useEffect(() => {
-    let route =
-      idx !== 0
-        ? fileInfo?.routes[idx - 1]?.path.map((pos) => pos[0])
-        : fileInfo?.currentRoute?.map((pos) => pos[0])
-    setRoute(route)
-  }, [idx, fileInfo?.currentRoute])
 
   return (
     <>
-      <MapBoxComponent location={[route[route.length - 1] ?? 0, '']}>
-        {route && (
-          <>
-            <MapboxGL.PointAnnotation coordinate={route[0] ?? location[0]} key="start" id="pt-ann">
-              <TamaIcon iconName="MapPin" color="$black10" size="$2" />
-            </MapboxGL.PointAnnotation>
-            <MapboxGL.PointAnnotation
-              coordinate={route.length ? route[route.length - 1] : location[0]}
-              key="end"
-              id="pt-ann"
-            >
-              <TamaIcon iconName="MapPin" color="$black10" size="$2" />
-            </MapboxGL.PointAnnotation>
-            <MapboxGL.ShapeSource
-              id="line-source"
-              shape={{
-                type: 'Feature',
-                properties: {},
-                geometry: {
-                  type: 'LineString',
-                  coordinates: route,
-                },
-              }}
-            >
-              <MapboxGL.LineLayer
-                id="line"
-                sourceID="line"
-                style={{
-                  lineColor: idx !== 0 ? fileInfo?.routes[idx - 1]?.lineColor : '#000',
-                  lineWidth: idx !== 0 ? fileInfo?.routes[idx - 1]?.lineWidth : 3,
-                }}
-              />
-            </MapboxGL.ShapeSource>
-          </>
-        )}
-      </MapBoxComponent>
       <Stack top={25} flex={1} zIndex={3} pos="absolute" width="100%" ai="center"></Stack>
       <Stack zIndex={3} pos="absolute" left={0} bottom={100}>
         <Carousel
@@ -155,6 +217,44 @@ export function RouteView() {
           <Button>현재 파일</Button>
         )}
       </XStack>
+    </>
+  )
+}
+
+const renderScreen = SceneMap({
+  first: RouteInfoView,
+  second: RouteListView,
+})
+export function RouteView() {
+  const { idx } = useMarkerState()
+  const layout = useWindowDimensions()
+  const [tabIdx, setTabIdx] = useState(1)
+  const [zIndex, setZIndex] = useState(1)
+  const [routes] = useState([
+    { key: 'first', title: 'info' },
+    { key: 'second', title: 'route' },
+  ])
+  useEffect(() => {}, [idx])
+  return (
+    <>
+      <RouteOnMap location={[127, 38]} />
+      <TabView
+        navigationState={{
+          index: tabIdx,
+          routes,
+        }}
+        style={{ width: '100%', height: '100%', zIndex: zIndex }}
+        renderScene={renderScreen}
+        onIndexChange={setTabIdx}
+        initialLayout={{ width: layout.width, height: layout.height }}
+      />
+      <Button
+        onPress={() => setZIndex(zIndex === 1 ? 10 : 1)}
+        backgroundColor={'$white10'}
+        zIndex={10}
+      >
+        press the map view
+      </Button>
     </>
   )
 }
